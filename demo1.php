0d7d1505f597fda76e3fae494723a8f0d6d9eb1d
斑马 <1908732958@qq.com>
2017-12-14 17:46:16 +0800

    key?

--- demo1.php
+++ demo1.php
@@ -4,3 +4,19 @@ Are you OK!
 Are you ? Thank you !
 Thank you very much!
  */
+ /* 
+SHA256:SrqNQGhYp5hdGfOREnwIvVN8fnrbyuic+3Y7WMn5bVM 1908732958@qq.com
+The key's randomart image is:
++---[RSA 2048]----+
+|  .+++..         |
+|    ===..        |
+|  . +=.o         |
+|.* +o   . .      |
+|=.+  .. So . o   |
+|..   o .. . =   E|
+|  . . .  . = . ..|
+|   . + . ++ + ..o|
+|    o ..*++o.o ..|
++----[SHA256]-----+
+
+ */
